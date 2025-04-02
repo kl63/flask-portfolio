d9bd181b3fcd32b4e4a674add80683ce278be0c0
Kevin Lin <lin.kevin.1923@gmail.com>
2025-04-02 15:01:01 -0400

    initial commit from pi

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "<h2>About Kevin</h2><p>This is your Flask-powered Pi server 🎉</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
